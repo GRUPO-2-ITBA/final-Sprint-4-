6d3d3cad0777661e02d2571f0e401bc9924e2599
MatiasLaguna <mlaguna@frd.utn.edu.ar>
2022-07-03 11:53:22 -0300

    Agrego rango fechas

--- listado_chesques.py
+++ listado_chesques.py
@@ -2,43 +2,56 @@ import csv
 import sys
 from datetime import datetime
 
-t1 = str(int(datetime.now().timestamp()))
-
 argumentos = sys.argv
-
-def estado(x):
-    return  x[-1] == data["estado"].upper() if data["estado"] != None else True
-
 
 data = {
     "csv": argumentos[1],
     "dni": argumentos[2],
     "salida": argumentos[3],
     "tipo": argumentos[4].upper(),
-    "estado": None,
-    "rangoFechas": argumentos[6] if len(argumentos) == 7 else None
+    "estado": True,
+    "rangoFechas": argumentos[6] if len(argumentos) == 7 else True
 }
 
-if len(argumentos) > 5:
+if len(argumentos) >= 5:
     data["rangoFechas" if ":" in argumentos[5] else "estado"] = argumentos[5]
-    
+
+
+
+
+def estado(x):
+     return x[-1] == data["estado"].upper() if (data["estado"] != True) else data["estado"]
+
+def fechas(x):
+    if data["rangoFechas"] != True:
+        rango = data["rangoFechas"].split(":")
+        fechaCSV = absoluteDate(int(x[-4]))
+        return fechaCSV >= absoluteDate(rango[0]) and fechaCSV <= absoluteDate(rango[1])
+    else:
+        return True
+
+def absoluteDate(date):
+    if type(date) == str:
+        date= datetime.timestamp(datetime.strptime(date, '%d-%m-%Y'))
+    return int(date/(60*60*24))
+
+
+
 file = open(data["csv"], "r", encoding='latin1')
-lineas = csv.reader(file)
-filtrado = [
+lineas = list(csv.reader(file))
+
+fild = [
     x for x in lineas
-    if x[-3] == data["dni"] and x[-2] == data["tipo"] and estado(x)
+    if x[-3] == data["dni"] and x[-2] == data["tipo"] and estado(x) and fechas(x)  
 ]
 file.close()
 
-if "salida "== 'CSV':
+if data["salida"] == 'CSV':
     timeStampActual = str(datetime.now().timestamp())
     f = open("dni" + '-' + timeStampActual + '.csv',"a") 
     fileCSV=csv.writer(f)
-    fileCSV.writerows(filtrado)
+    fileCSV.writerows(fild)
     f.close()
-#print(filtrado)....
-#for x in range(len(filtrado)):
-#print filtrado[x],
-#No funciona por que no es un string xD
- print(filtrado)
+else:
+    print(fild)
 
